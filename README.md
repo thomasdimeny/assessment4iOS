45feebe46e86e5117221bff7a307bc1263fa9f20
Thomas Dimeny <thomasdimeny@Interapts-MacBook-Pro-107.local>
2018-05-08 15:35:18 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# assessment4iOS
---
+++ Week4Assessment/ApiManager.swift
@@ -0,0 +1,9 @@
+//
+//  ApiManager.swift
+//  Week4Assessment
+//
+//  Created by Thomas Dimeny on 5/8/18.
+//  Copyright © 2018 Ryan Brashear. All rights reserved.
+//
+
+import Foundation
